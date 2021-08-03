fad94fe6b13404dafdea1ddb77d276e3b170f998
mcrosey <80073115+mcrosey@users.noreply.github.com>
2021-08-03 11:42:00 -0400

    firebase hosting added

---
+++ clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcu6jG4SrF_n7P9SWsW7e6SzzxFrck9lM",
+    authDomain: "whatsappclone-4d857.firebaseapp.com",
+    projectId: "whatsappclone-4d857",
+    storageBucket: "whatsappclone-4d857.appspot.com",
+    messagingSenderId: "984346855421",
+    appId: "1:984346855421:web:25fbe6e4213e47c1ad3b94",
+    measurementId: "G-KK8RNDQXB5"
+  };
