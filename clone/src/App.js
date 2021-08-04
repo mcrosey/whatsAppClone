9f782e99d7c0f4d41f967402d853bb0d68e01859
mcrosey <80073115+mcrosey@users.noreply.github.com>
2021-08-04 11:09:55 -0400

    material ui install

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,10 +1,12 @@
 import react from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="app">
       <div className="app-body"></div>
+      <Sidebar />
 
     </div>
   );
---
+++ clone/src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-header">
+
+            </div>
+            <div className="sidebar-search">
+
+            </div>
+            <div className="sidebar-chats">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
